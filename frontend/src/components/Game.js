d40e2a79c4925c5e72bae8a0e735ac4c05635a05
FerminLuceroJ <felucero@frba.utn.edu.ar>
2025-01-17 15:49:30 -0300

    fix problemas game.js

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -1,82 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback} from 'react';
 import countriesData from '../data/countries.json';
 import CountryFlag from './CountryFlag';
-
-const countries = {
-    Easy: [
-        'US', 'CA', 'GB', 'DE', 'FR', 'IT', 'ES', 'AU', 'BR', 'IN',
-        'CN', 'JP', 'RU', 'MX', 'AR', 'CL', 'CO', 'PE', 'ZA', 'EG',
-        'TR', 'KR', 'ID', 'TH', 'SA', 'SE', 'NO', 'FI', 'NZ', 'NL',
-        'PT', 'CH', 'IE', 'PL', 'GR', 'BE', 'AT', 'DK', 'CZ', 'HU',
-        'IL', 'MY', 'VN', 'PH', 'SG', 'HK', 'TW', 'UA', 'ZA', 'SK'
-    ],
-    Medium: [
-        'US', 'CA', 'GB', 'DE', 'FR', 'IT', 'ES', 'AU', 'BR', 'IN',
-        'CN', 'JP', 'RU', 'MX', 'AR', 'CL', 'CO', 'PE', 'ZA', 'EG',
-        'TR', 'KR', 'ID', 'TH', 'SA', 'SE', 'NO', 'FI', 'NZ', 'NL',
-        'PT', 'CH', 'IE', 'PL', 'GR', 'BE', 'AT', 'DK', 'CZ', 'HU',
-        'IL', 'MY', 'VN', 'PH', 'SG', 'HK', 'TW', 'UA', 'ZA', 'SK',
-        'NG', 'PK', 'BD', 'IR', 'IQ', 'KE', 'TZ', 'ZM', 'ZW', 'BO',
-        'DO', 'CR', 'GT', 'HN', 'SV', 'CU', 'VE', 'PY', 'UY', 'DZ',
-        'MA', 'TN', 'GH', 'CI', 'SN', 'UG', 'RW', 'BG', 'RO', 'LV',
-        'LT', 'EE', 'IS', 'MT', 'CY', 'LU', 'LI', 'MC', 'SM', 'VA'
-    ],
-    Hard: [
-        'AF', 'AL', 'DZ', 'AS', 'AD', 'AO', 'AI', 'AQ', 'AG', 'AR',
-        'AM', 'AW', 'AU', 'AT', 'AZ', 'BS', 'BH', 'BD', 'BB', 'BY',
-        'BE', 'BZ', 'BJ', 'BM', 'BT', 'BO', 'BA', 'BW', 'BR', 'BN',
-        'BG', 'BF', 'BI', 'KH', 'CM', 'CA', 'CV', 'KY', 'CF', 'TD',
-        'CL', 'CN', 'CO', 'KM', 'CG', 'CD', 'CK', 'CR', 'HR', 'CU',
-        'CY', 'CZ', 'DK', 'DJ', 'DM', 'DO', 'EC', 'EG', 'SV', 'GQ',
-        'ER', 'EE', 'SZ', 'ET', 'FJ', 'FI', 'FR', 'GA', 'GM', 'GE',
-        'DE', 'GH', 'GR', 'GD', 'GT', 'GN', 'GW', 'GY', 'HT', 'HN',
-        'HU', 'IS', 'IN', 'ID', 'IR', 'IQ', 'IE', 'IL', 'IT', 'JM',
-        'JP', 'JO', 'KZ', 'KE', 'KI', 'KP', 'KR', 'KW', 'KG', 'LA',
-        'LV', 'LB', 'LS', 'LR', 'LY', 'LI', 'LT', 'LU', 'MG', 'MW',
-        'MY', 'MV', 'ML', 'MT', 'MR', 'MU', 'MX', 'FM', 'MD', 'MC',
-        'MN', 'ME', 'MA', 'MZ', 'MM', 'NA', 'NR', 'NP', 'NL', 'NZ',
-        'NI', 'NE', 'NG', 'MK', 'NO', 'OM', 'PK', 'PW', 'PS', 'PA',
-        'PG', 'PY', 'PE', 'PH', 'PL', 'PT', 'QA', 'RO', 'RU', 'RW',
-        'KN', 'LC', 'VC', 'WS', 'SM', 'ST', 'SA', 'SN', 'RS', 'SC',
-        'SL', 'SG', 'SK', 'SI', 'SB', 'SO', 'ZA', 'SS', 'ES', 'LK',
-        'SD', 'SR', 'SE', 'CH', 'SY', 'TW', 'TJ', 'TZ', 'TH', 'TL',
-        'TG', 'TO', 'TT', 'TN', 'TR', 'TM', 'TV', 'UG', 'UA', 'AE',
-        'GB', 'US', 'UY', 'UZ', 'VU', 'VE', 'VN', 'YE', 'ZM', 'ZW'
-    ]
-};
-
-const questionLimits = {
-    Easy: 50,
-    Medium: 100,
-    Hard: 200
-};
 
 function Game({ difficulty, language, onBackToMenu }) {
     const [selectedCountry, setSelectedCountry] = useState('');
     const [selectedCountryCode, setSelectedCountryCode] = useState('');
     const [options, setOptions] = useState([]);
-    const [points, setPoints] = useState(1);
+    const [points, setPoints] = useState(1); // Start from 1
     const [correctAnswers, setCorrectAnswers] = useState(0);
     const [incorrectAnswers, setIncorrectAnswers] = useState(0);
     const [time, setTime] = useState(0);
-    const [questionsAsked, setQuestionsAsked] = useState(0);
+    const [questionsAsked, setQuestionsAsked] = useState(0); // Start from 0
     const [gameOver, setGameOver] = useState(false);
     const [askedCountries, setAskedCountries] = useState([]);
 
-    useEffect(() => {
-        generateQuestion();
-    }, [difficulty]);
-
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setTime(prevTime => prevTime + 1);
-        }, 1000);
-        if (gameOver) clearInterval(timer);
-        return () => clearInterval(timer);
-    }, [gameOver]);
-
-    const generateQuestion = () => {
-        if (questionsAsked >= questionLimits[difficulty]) {
+    const generateQuestion = useCallback(() => {
+        if (questionsAsked >= questionLimits[difficulty] + 1) {
             setGameOver(true);
             return;
         }
@@ -88,7 +27,8 @@ function Game({ difficulty, language, onBackToMenu }) {
         }
 
         const randomCountry = countryList[Math.floor(Math.random() * countryList.length)];
-        const incorrectOptions = countries[difficulty].filter(country => country !== randomCountry)
+        const incorrectOptions = countries[difficulty]
+            .filter(country => country !== randomCountry)
             .sort(() => 0.5 - Math.random())
             .slice(0, 3);
         const allOptions = [...incorrectOptions, randomCountry].sort(() => 0.5 - Math.random());
@@ -103,7 +43,21 @@ function Game({ difficulty, language, onBackToMenu }) {
         setOptions(allOptions.map(getCountryName));
         setQuestionsAsked(prevQuestions => prevQuestions + 1);
         setAskedCountries(prevAsked => [...prevAsked, randomCountry]);
-    };
+    }, [questionsAsked, difficulty, askedCountries, language]);
+
+    useEffect(() => {
+        generateQuestion();
+    }, [generateQuestion]);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(prevTime => prevTime + 1);
+        }, 1000);
+        if (gameOver) {
+            clearInterval(timer);
+        }
+        return () => clearInterval(timer);
+    }, [gameOver]);
 
     const handleOptionClick = (option) => {
         if (option === selectedCountry) {
@@ -119,11 +73,11 @@ function Game({ difficulty, language, onBackToMenu }) {
         setSelectedCountry('');
         setSelectedCountryCode('');
         setOptions([]);
-        setPoints(1);
+        setPoints(1); // Reset to 1
         setCorrectAnswers(0);
         setIncorrectAnswers(0);
         setTime(0);
-        setQuestionsAsked(0);
+        setQuestionsAsked(0); // Reset to 0
         setGameOver(false);
         setAskedCountries([]);
         generateQuestion();
